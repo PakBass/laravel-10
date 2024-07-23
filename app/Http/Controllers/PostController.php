c75b59ff3c52d761ba6069ab284fa66648cd2ce8
PakBass <andhibasuki@gmail.com>
2024-07-23 13:16:25 +0800

    Menambahkan insert data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
+    public function index() : View
     {
         $posts = Post::latest()->paginate(3);
         return view('posts.index', compact('posts'));
+    }
+
+    //Membuat function create untuk menampilkan form tambah data
+    public function create() : View
+    {
+        return view('posts.create');
+    }
+
+    //Membuat function store untuk memproses data ke database dan upload gambar
+    public function store(Request $request) : RedirectResponse
+    {
+        //Membuat validasi form
+        $this->validate($request, [
+            'image'     => 'required|image|mimes:png,jpg,jpeg|max:2040',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]);
+
+        //Upload gambar
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //Simpan ke database
+        Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        //redirect ke halaman index
+        return redirect()->route('posts.index')->with([
+            'success' => 'Data berhasil disimpan'
+        ]);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,17 +1,20 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Belajar CRUD Laravel 10</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-  </head>
-  <body style="background: lightgray">
-    <div class="container">
+</head>
+
+<body style="background: lightgray">
+    <div class="container mt-5">
         <div class="row">
-            <div class="col">
+            <div class="col-md-12">
                 <div class="">
                     <h3 class="text-center my-4">Data Postingan Berita SKARLA</h3>
                 </div>
@@ -20,31 +23,35 @@
                         <a href="{{ route('posts.create') }}" class="btn btn-success mb-3">TAMBAH BERITA</a>
                         <table class="table table-bordered">
                             <thead>
-                                <tr>
+                                <tr class="text-center">
                                     <th scope="col">GAMBAR</th>
-                                    <th scope="col">IMAGE</th>
-                                    <th scope="col">TITLE</th>
-                                    <th scope="col">CONTENT</th>
+                                    <th scope="col">JUDUL</th>
+                                    <th scope="col">KONTEN</th>
+                                    <th scope="col">AKSI</th>
                                 </tr>
                             </thead>
                             <tBody>
                                 @forelse ($posts as $post)
-                                <tr>
-                                    <td class="text-center">
-                                        <img src="{{ asset('/storage/posts/'.$post->image) }}" class="rounded" style="150px" alt="">
-                                    </td>
-                                    <td>{{ $post->title }}</td>
-                                    <td>{{ $post->content }}</td>
-                                    <td class="text-center">
-                                        <form action="{{ route('posts.destroy', $post->id) }}" method="POST" onclick="return confirm('Apakah anda yakin ?');">
-                                            <a href="{{ route('posts.show', $post->id) }}" class="btn btn-dark">View</a>
-                                            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-warning">Edit</a>
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-sm btn-danger">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
+                                    <tr>
+                                        <td class="text-center">
+                                            <img src="{{ asset('/storage/posts/' . $post->image) }}" class="rounded"
+                                                style="150px" alt="">
+                                        </td>
+                                        <td>{{ $post->title }}</td>
+                                        <td>{!! $post->content !!}</td>
+                                        <td class="text-center">
+                                            <form action="{{ route('posts.destroy', $post->id) }}" method="POST"
+                                                onclick="return confirm('Apakah anda yakin ?');">
+                                                <a href="{{ route('posts.show', $post->id) }}"
+                                                    class="btn btn-dark">View</a>
+                                                <a href="{{ route('posts.edit', $post->id) }}"
+                                                    class="btn btn-warning">Edit</a>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
 
                                 @empty
                                     <div class="alert alert-danger">
@@ -59,17 +66,23 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-    <script>
-        @if (session()->has('success'))
-
-            toastr.success('{{ session('success') }}', 'BERHASIL!');
-        @elseif (session()->has('error'))
-
-            toastr.error('{{ session('error') }}', 'GAGAL!');
-
-        @endif
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
-  </body>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    @if(session('success'))
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Data Berhasil DIsimpan',
+                    timer: 3000,
+                    showConfirmButton: false
+                });
+            });
+        </script>
+    @endif
+</body>
+
 </html>
